eef64d38bf31622fc646f2590064258fcf0fcd0f
Marks Polakovs <marks@markspolakovs.me>
2017-07-31 18:38:34 +0300

    Positions page (initial)

--- resources/views/positions.blade.php
+++ resources/views/positions.blade.php
@@ -3,5 +3,60 @@
 @section('title', 'Positions')
 
 @section('content')
-  todo put stuff here
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <div class="page-header">
+          <h1>Positions</h1>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-12">
+        <h2>President</h2>
+        <p>The President is the executive of the Pupils’ Committee. Their role is to run the Committee smoothly and efficiently, as well as represent the points of view of the students to the best of their ability. They must make sure everything is running well and everyone is doing their job. Becoming President is not easy, but neither is being President. Candidates must be sure they can take on the responsibility.</p>
+        <p><b>Note: you must be at least 16 years old to apply for the Presidency</b></p>
+
+        <h2>Vice-President</h2>
+        <p>The Vice-President’s role is to assist the President as much as they can, to ease their workload. They must know as much as possible about the functioning of the PC, and actively participate in its work.</p>
+
+        <p><b>Note: you must be at least 16 years old to apply for the Presidency</b></p>
+
+        <h2>Internal Committee</h2>
+        <h3>Head of Internal</h3>
+        <p>The Head of Internal oversees internal affairs within the school. They, along with the Internal Committee, must book rooms when needed, assist with sales, posters, organising class rep meetings, etcetera. </p>
+        <h3>Poster Designers</h3>
+        <p>The Poster Designers <b>(2 persons)</b>, along with the Head of Internal, form the Internal Committee. They design posters for the PC’s various activities. In addition to this, they assist the Head of Internal with their work, should they need it.</p>
+
+        <h2>Secretary</h2>
+        <p>The Secretary prepares the minutes of all PC meetings and takes note of absences from meetings. They also organise the flow of documents around the PC, whether on paper or digitally (which is fancy-speak for “read emails”).</p>
+
+        <h2>Treasurer</h2>
+        <p>The Treasurers <b>(2 persons)</b> oversee the PC’s finances. They must work with the President and ensure that the PC’s financial situation is under control.</p>
+
+        <h2>CoSup</h2>
+        <p>CoSup stands for <em>"Conseil Supérieur des Elèves"</em>. The best way to think about it is as a "Committee of Committees". Each European School sends several representatives to meetings with the others, which are usually four times a week in either Luxembourg or Brussels. The CoSup members <b>(2 persons)</b> attend the CoSup meetings, and discuss the PC’s position with the PCs of the other European Schools.</p>
+
+        <p><b>Note: you must be at least 16 years old to apply for CoSup.</b></p>
+
+        <h2>Internal Relations</h2>
+        <p>The Internal Relations member communicates with the students of the school and their groups and/or clubs. They are charged with relations between the PC and these groups, and to assist them in case they need it (financial support, advertising, etc.).</p>
+
+        <h2>Conseil Representatives</h2>
+        <p>The Conseil Representatives <b>(3 people)</b> attend the meetings of the Conseil d’Education, Conseil Consultatif, Canteen Committee, and Library Committee, where they represent students’ interests, and keep the PC and the students informed of what is happening. They should divide up the work between them. </p>
+
+        <h2>IT Member</h2>
+        <p>The IT Member runs the PC's website and Facebook page. They are in charge of anything that needs to be done online (for example, public voting, or this very website).</p>
+
+        <h2>Leisure Committee</h2>
+        <p>The Leisure Committee <b>(4 persons)</b> organise the parties, discos, and other events throughout the year. They must remain in close contact with the rest of the PC as well as the Mamer PC.</p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div class="col-md-12">
+        <p>It should be noted that members aren't constrained to their responsibilities, and often do more than they are required to do. In addition to their duties stated above, members must support the other members as much as possible, especially when the workload is severely imbalanced.</p>
+      </div>
+    </div>
+  </div>
 @endsection
