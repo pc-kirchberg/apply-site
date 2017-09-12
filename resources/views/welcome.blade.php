47ac4b2b13d0e0cc6b1fdd0a30e8f60040cd45dc
Marks Polakovs <marks@markspolakovs.me>
2017-09-12 21:43:36 +0200

    Add PowerPoint embed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,5 +20,10 @@
         <a class="btn btn-primary frontpage-button" href="{{ route('form') }}">Apply</a>
       </div>
     </div>
+    <div class="row">
+      <div class="col-md-12">
+        <iframe src="https://drive.google.com/file/d/0BxEHthlhxeTcdVVPLXRvc3lXVVk/preview" width="640" height="480"></iframe>
+      </div>
+    </div>
   </div>
 @endsection
